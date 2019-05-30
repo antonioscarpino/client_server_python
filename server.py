e5a74819c236c7defb9c53a954c7f9867ed16ee8
antonioscarpino <antonio.scarpino@gmail.com>
2019-05-30 07:19:57 +0200

    server

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+import subprocess
+import sys
+
+# configurazione del server
+url = ""  # indirizzo IP a cui bindare il server
+port = 15000  # porta sulla quale risponde il server
+
+
+def ricevi_comandi(url, port, conn, s):
+    pass
+
+
+def sub_server(url, port, backlog=1):
+    pass
+
+
+if __name__ == '__main__':
+    sub_server(url, port)
